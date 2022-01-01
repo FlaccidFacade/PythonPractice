8cb74a505cce9caa4ffd5cfc2b2efe5ca1a0c468
FlaccideFacade <zachary.glenn.turner@gmail.com>
2022-01-01 15:46:02 -0500

    divisors problem

---
+++ practiceProblems/src/divisors/get_divisors.py
@@ -0,0 +1,23 @@
+"""Create a program that asks the user for a number and 
+then prints out a list of all the divisors of that number. 
+(If you don’t know what a divisor is, it is a number that 
+divides evenly into another number. For example, 13 is a 
+divisor of 26 because 26 / 13 has no remainder.)"""
+
+
+
+def get(numerator:int==0) -> list:
+    """gets the list of divisors for the given numerator
+
+    Args:
+        numerator (int, optional): The value we are looking divisors of. Defaults to =0.
+
+    Returns:
+        list: The list of divisors found
+    """
+    l = []
+    for i in range(1,numerator):
+        if numerator % i == 0:
+            l.append(i)
+
+    return l
---
+++ practiceProblems/tests/divisors/test_get_divisors.py
@@ -0,0 +1,11 @@
+
+import divisors.get_divisors as divisors
+
+def test_get_divisors():
+    #test to make sure the proper divisors are returned
+    #  by spot checking different values
+    divs = divisors.get(26)
+    assert divs == [1,2,13]
+
+    divs = divisors.get(100)
+    assert divs == [1,2,4,5,10,20,25,50]
